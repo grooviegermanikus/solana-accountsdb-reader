642017de7df7d3250f0d547f7bcd2a038c8ace75
GroovieGermanikus <groovie@mango.markets>
2024-06-20 16:25:18 +0200

    implement owner+account partkey btree

--- src/bin/solana-accountsdb-reader/main.rs
+++ src/bin/solana-accountsdb-reader/main.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
+use std::hash::Hasher;
 use std::path::PathBuf;
 use std::str::FromStr;
 use cap::Cap;
@@ -20,6 +21,9 @@ use {
     },
 };
 use clap::Parser;
+use fnv::FnvHasher;
+use itertools::Itertools;
+use modular_bitfield::prelude::B31;
 use solana_accounts_db::account_info::{AccountInfo, StorageLocation};
 use solana_sdk::pubkey::Pubkey;
 use solana_accountsdb_reader::parallel::AppendVecConsumer;
@@ -56,6 +60,7 @@ async fn main() -> anyhow::Result<()> {
     info!("Reading snapshot from {:?} with {} bytes", archive_path, archive_path.metadata().unwrap().len());
 
     let mut loader: ArchiveSnapshotExtractor<File> = ArchiveSnapshotExtractor::open(&archive_path).unwrap();
+    info!("... opened snapshot archive");
 
     // for vec in loader.iter() {
     //     let append_vec =  vec.unwrap();
@@ -69,32 +74,71 @@ async fn main() -> anyhow::Result<()> {
 
     let before = ALLOCATOR.allocated();
 
-    let mut store = HashMapMapStore::new();
+    let mut store_hashmapmap = HashMapMapStore::new();
+    let mut store_btree = ProgramPrefixBtree::new();
     for snapshot_result in loader.iter() {
         for append_vec in snapshot_result {
-            info!("size: {:?}", append_vec.len());
-            info!("slot: {:?}", append_vec.slot());
+            trace!("size: {:?}", append_vec.len());
+            trace!("slot: {:?}", append_vec.slot());
             for handle in append_vec_iter(&append_vec) {
                 let stored = handle.access().unwrap();
                 trace!("account {:?}: {}", stored.meta.pubkey, stored.account_meta.lamports);
                 let stuff = AccountStuff {
                 };
-                store.store(stored.meta.pubkey, stuff);
+                store_hashmapmap.store(stored.meta.pubkey, stored.account_meta.owner, stuff);
+                let stuff = AccountStuff {
+                };
+                store_btree.store(stored.meta.pubkey, stored.account_meta.owner, stuff);
             }
         }
     }
 
     let after = ALLOCATOR.allocated();
 
-    info!("HEAP allocated: {} ({:.2}/acc)", after - before, (after - before) as f64 / store.size() as f64);
+    info!("HEAP allocated: {} ({:.2}/acc)", after - before, (after - before) as f64 / store_hashmapmap.total_count() as f64);
 
-    store.debug();
+    store_hashmapmap.debug();
+    store_btree.debug();
 
     Ok(())
 }
 
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct MagicKey {
+    owner_pubkey_part: u32,
+    account_pubkey_part: u32,
+}
+
+struct ExpandedKey {
+    pub owner_pubkey: Pubkey,
+    pub account_pubkey: Pubkey,
+}
+
+impl MagicKey {
+    fn from_pubkeys(owner_pubkey: Pubkey, account_pubkey: Pubkey) -> Self {
+        let mut hasher = FnvHasher::default();
+        hasher.write(account_pubkey.as_ref());
+        let owner_hash64 = hasher.finish();
+
+        let owner_pubkey_part = owner_hash64 as u32;
+        let account_pubkey_part = prefix_from_pubkey(&account_pubkey);
+        Self {
+            owner_pubkey_part,
+            account_pubkey_part,
+        }
+    }
+}
+
+fn prefix_from_pubkey(pubkey: &Pubkey) -> u32 {
+    const OFFSET: usize = 4;
+    const INFIX_SIZE: usize = 4;
+    u32::from_be_bytes(pubkey.as_ref()[OFFSET..OFFSET + INFIX_SIZE].try_into().unwrap())
+}
+
+
 struct HashMapMapStore {
-    store: HashMap<Pubkey, AccountStuff>,
+    // owner/program pubkey -> account pubkey -> account stuff
+    store: HashMap<Pubkey, HashMap<Pubkey, AccountStuff>>,
     writes: usize,
     overwrites: usize,
 }
@@ -108,20 +152,75 @@ impl HashMapMapStore {
         }
     }
 
-    fn store(&mut self, key: Pubkey, value: AccountStuff) {
+    fn store(&mut self, account_pubkey: Pubkey, owner_pubkey: Pubkey, value: AccountStuff) {
         self.writes += 1;
-        let update = self.store.insert(key, value);
-        if update.is_some() {
+
+        let replacement = self.store.entry(owner_pubkey)
+            .or_insert_with(HashMap::new) // TODO set default capacity
+            .insert(account_pubkey, value);
+
+        if replacement.is_some() {
             self.overwrites += 1;
         }
     }
 
-    fn size(&self) -> usize {
+    fn owner_count(&self) -> usize {
+        self.store.len()
+    }
+
+    // note: this is slow
+    fn total_count(&self) -> usize {
+        self.store.values().map(|m| m.len()).sum()
+    }
+
+    fn debug(&self) {
+        info!("HashMap, owners: {}, total: {}, writes: {}, overwrites: {}",
+            self.owner_count(), self.total_count(), self.writes, self.overwrites);
+    }
+}
+
+struct ProgramPrefixBtree {
+    // TODO add account_key + owner_key to detect collisions
+    store: BTreeMap<MagicKey, (ExpandedKey, AccountStuff)>,
+    writes: usize,
+    overwrites: usize,
+}
+
+impl ProgramPrefixBtree {
+    fn new() -> Self {
+        Self {
+            store: BTreeMap::new(),
+            writes: 0,
+            overwrites: 0,
+        }
+    }
+
+    fn store(&mut self, account_pubkey: Pubkey, owner_pubkey: Pubkey, value: AccountStuff) {
+        self.writes += 1;
+
+        let magic_key = MagicKey::from_pubkeys(owner_pubkey, account_pubkey);
+        let expanded_key = ExpandedKey {
+            owner_pubkey,
+            account_pubkey,
+        };
+        let replacement = self.store.insert(magic_key, (expanded_key, value));
+
+        if let Some(prev_value) = replacement {
+            self.overwrites += 1;
+
+            assert_eq!(prev_value.0.owner_pubkey, owner_pubkey, "owner pubkey fnv hash collision");
+            assert_eq!(prev_value.0.account_pubkey, account_pubkey, "account pubkey infix collision");
+
+        }
+    }
+
+    fn count(&self) -> usize {
         self.store.len()
     }
 
     fn debug(&self) {
-        info!("HashMap, entries: {}, writes: {}, overwrites: {}", self.size(), self.writes, self.overwrites);
+        info!("BTreeMap, count: {}, writes: {}, overwrites: {}",
+            self.count(), self.writes, self.overwrites);
     }
 }
 
@@ -186,6 +285,16 @@ impl AppendVecConsumer for SimpleLogConsumer {
         }
         Ok(())
     }
+}
+
+
+#[test]
+fn test_magic_key() {
+    let owner_pubkey = Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
+    let account_pubkey = Pubkey::from_str("12o5TrTwDfsACu7oCBjeBZ8oVDSfv2UW2cLvR8c47zdf").unwrap();
+    let key = MagicKey::from_pubkeys(owner_pubkey, account_pubkey);
+    assert_eq!(key.owner_pubkey_part, 6);
+    assert_eq!(key.account_pubkey_part, 0);
 }
 
 
