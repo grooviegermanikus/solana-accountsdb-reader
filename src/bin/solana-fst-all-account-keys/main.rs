d7e10201a7150e283e885000f8849abd334cd5c2
GroovieGermanikus <groovie@mango.markets>
2024-07-15 23:29:16 +0200

    scale to 256 bins

--- src/bin/solana-fst-all-account-keys/main.rs
+++ src/bin/solana-fst-all-account-keys/main.rs
@@ -3,10 +3,14 @@ use std::io;
 use std::path::PathBuf;
 use std::process::exit;
 use std::str::FromStr;
+use std::sync::{Arc, RwLock};
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::time::Instant;
+use std::vec::IntoIter;
 use clap::Parser;
 use fst::{Automaton, IntoStreamer, Map, MapBuilder, SetBuilder, Streamer};
 use fst::automaton::{StartsWith, Str, Subsequence};
+use futures::future::join_all;
 use indexmap::IndexSet;
 use indexmap::set::Iter;
 use itertools::Itertools;
@@ -41,8 +45,14 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // TODO replace with external sorter (see merge.rs in fst project)
 
-    let mut index_set: IndexSet<[u8; 32]> = IndexSet::with_capacity(100_000_000);
+    const BINS: usize = 256;
+    let mut bins = vec![];
+    for _ in 0..BINS {
+        let index_set: IndexSet<[u8; 32]> = IndexSet::with_capacity(1_000_000);
+        bins.push(Arc::new(RwLock::new(index_set)));
+    }
 
+    let mut total_accounts: usize = 0;
     info!("Loading all account keys from the snapshot");
     let started_at = Instant::now();
     for vec in loader.iter() {
@@ -50,30 +60,46 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         // info!("size: {:?}", append_vec.len());
         for account_keys_chunk in &append_vec_iter(&append_vec)
             .map(|ref handle| handle.access().unwrap().meta.pubkey.to_bytes())
-            .chunks(1024)
+            .chunks(1024 * 1024)
         {
-            // for key in account_keys_chunk {
-            //     info!("key: {:?}", key);
-            // }
-            index_set.extend(account_keys_chunk);
+            account_keys_chunk.group_by(|key| key[0] as usize % BINS)
+                .into_iter()
+                .for_each(|(bin_idx, keys)| {
+                    let mut lk = bins[bin_idx].write().unwrap();
+                    lk.extend(keys);
+                    // info!("bin {} len {}", bin_idx, lk.len());
+                });
 
         }
     }
-    info!("loaded all {} account keys into indexset in {:.1}ms",
-        index_set.len(),
-        started_at.elapsed().as_millis());
+    total_accounts = 41;
+    let count_per_bin = bins.iter().map(|bin| bin.read().unwrap().len()).collect::<Vec<_>>();
+    info!("loaded all {} account keys into indexsets in {:.1}ms: {:?}",
+        total_accounts,
+        started_at.elapsed().as_millis(), count_per_bin);
 
-    let started_at = Instant::now();
-    index_set.sort_by(|lhs, rhs| lhs.cmp(rhs));
-    let elapsed = started_at.elapsed();
-    info!("sorted indexset in {:.1}ms", elapsed.as_millis());
+    let mut bin_cnt = Arc::new(AtomicU32::new(0));
+    let mut threads = vec![];
+    for bin in bins {
+        let bin_cnt = bin_cnt.clone();
+        let jh = std::thread::spawn(move || {
+            let bin_file = format!("storage/bin-{}.fst", bin_cnt.fetch_add(1, Ordering::Relaxed));
+            let lk = bin.write().unwrap();
+            let started_at = Instant::now();
+            // lk.sorted_by(|a, b| a.cmp(b));
+            info!("Building FST index file {} ...", bin_file);
+            build_index_fst_file(&bin_file, lk.iter().sorted().into_iter()).unwrap(); // TODO handle panic
+            info!("... built FST index file {} in {}ms", bin_file, started_at.elapsed().as_millis());
+        });
+        threads.push(jh);
+    }
 
-    let started_at = Instant::now();
-    info!("Building FST index file {} ...", INDEX_FILE);
-    build_index_fst_file(index_set.iter())?;
-    info!("... built FST index file {} in {}ms", INDEX_FILE, started_at.elapsed().as_millis());
 
-    scan_it()?;
+    for jh in threads {
+        jh.join().unwrap();
+    }
+
+    // scan_it()?;
 
     Ok(())
 }
@@ -90,13 +116,13 @@ pub struct Args {
 
 const INDEX_FILE: &'static str = "storage/all-account-pubkeys.fst";
 
-fn build_index_fst_file(pubkey_sorted: Iter<[u8; 32]>) -> Result<(), Box<dyn std::error::Error>> {
+fn build_index_fst_file(index_file: &str, pubkey_sorted: IntoIter<&[u8; 32]>) -> Result<(), Box<dyn std::error::Error>> {
 
     let fst_out_file = OpenOptions::new()
         .write(true)
         .create(true)
         .truncate(true)
-        .open(INDEX_FILE)
+        .open(index_file)
         .unwrap();
 
     let wrt = io::BufWriter::with_capacity(16 * 1024 * 1024, fst_out_file);
