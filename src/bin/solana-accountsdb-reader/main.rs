9f11277725a90f893568ce44a3bd5e8a147b2f2e
GroovieGermanikus <groovie@mango.markets>
2024-07-15 10:41:52 +0200

    write using pickled+array+mmap

--- src/bin/solana-accountsdb-reader/main.rs
+++ src/bin/solana-accountsdb-reader/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::time::Instant;
@@ -28,6 +29,8 @@ use std::collections::HashMap;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::{BufWriter, Write};
+use fst::{IntoStreamer, Set, SetBuilder, Streamer};
+use memmap2::MmapMut;
 use solana_accountsdb_reader::parallel_io::pwrite_all;
 
 #[global_allocator]
@@ -55,7 +58,7 @@ async fn main() -> anyhow::Result<()> {
     let mut index_map = IndexMap::<[u8; 32], ()>::new();
     let mut trie: Trie<[u8; 32], ()> = Trie::new();
 
-    let program_filter = Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
+    // let program_filter = Pubkey::from_str("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA").unwrap();
 
     info!("Reading the snapshot...");
     let started_at = Instant::now();
@@ -69,13 +72,15 @@ async fn main() -> anyhow::Result<()> {
             let stored = handle.access().unwrap();
             let account_key = stored.meta.pubkey;
             let program_key = stored.account_meta.owner;
-            if program_key != program_filter {
-                continue;
-            }
+            // if program_key != program_filter {
+            //     continue;
+            // }
             // trie.insert(account_key.to_bytes(), ());
             index_map.insert(account_key.to_bytes(), ());
         }
     }
+
+
 
     let after = ALLOCATOR.allocated();
     let elapsed = started_at.elapsed();
@@ -104,7 +109,28 @@ async fn main() -> anyhow::Result<()> {
     }
     info!("iterated over trie with {} items in {:.1}ms", count, started_at.elapsed().as_millis());
 
+    let started_at = Instant::now();
+    index_map.sort_by(|a, b, x, y| a.cmp(x));
+    let elapsed = started_at.elapsed();
+    info!("sorted indexmap in {:.1}ms", elapsed.as_millis());
 
+    let started_at = Instant::now();
+    let mut fst_builder = SetBuilder::memory();
+    fst_builder.extend_iter(index_map.keys().map(|pk| pk.as_ref())).unwrap();
+    // index_map.keys().for_each(|pk| {
+    //     fst_builder.insert(pk).unwrap();
+    // });
+    let fst_bytes = fst_builder.into_inner().unwrap();
+    let elapsed = started_at.elapsed();
+    info!("built fst size in {} bytes in {:.1}ms", fst_bytes.len(), elapsed.as_millis());
+
+
+    let set = Set::new(fst_bytes).unwrap();
+    info!("fst set size: {:?}", set.len());
+    let mut stream = set.range().ge([42,00]).lt([42,00,20]).into_stream();
+    while let Some(key) = stream.next() {
+        info!("range match: {:?}", key);
+    }
 
     // let blob = bincode::serialize(&trie).unwrap();
     // info!("serialized trie to {} bytes ({:.1}bytes/item)", blob.len(), blob.len() as f64 / trie.count() as f64);
@@ -128,15 +154,23 @@ async fn main() -> anyhow::Result<()> {
         .unwrap();
 
 
-
     let started_at = Instant::now();
     let serialized = serde_pickle::to_vec(&index_map, Default::default()).unwrap();
-    pwrite_all(&file, &serialized, 0).unwrap();
-
-    let file_size = fs::metadata(FILE).unwrap().len();
+    let mut mmap_file = MmapMut::map_anon(serialized.len())?;
+    mmap_file.copy_from_slice(&serialized);
+    mmap_file.flush().unwrap();
     info!("serialized indexmap to {} bytes ({:.1}bytes/item) took {:.1}ms",
-        file_size, file_size as f64 / index_map.len() as f64,
+        serialized.len(), serialized.len() as f64 / index_map.len() as f64,
         started_at.elapsed().as_millis());
+
+    // let started_at = Instant::now();
+    // let serialized = serde_pickle::to_vec(&index_map, Default::default()).unwrap();
+    // pwrite_all(&file, &serialized, 0).unwrap();
+    //
+    // let file_size = fs::metadata(FILE).unwrap().len();
+    // info!("serialized indexmap to {} bytes ({:.1}bytes/item) took {:.1}ms",
+    //     file_size, file_size as f64 / index_map.len() as f64,
+    //     started_at.elapsed().as_millis());
 
 
     Ok(())
