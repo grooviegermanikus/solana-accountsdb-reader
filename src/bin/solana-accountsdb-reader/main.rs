c6b36c3df5b0bd50cf1d9275c0c069d1256dfc00
GroovieGermanikus <groovie@mango.markets>
2024-07-18 10:22:00 +0200

    compile

--- src/bin/solana-accountsdb-reader/main.rs
+++ src/bin/solana-accountsdb-reader/main.rs
@@ -247,7 +247,8 @@ async fn main() -> anyhow::Result<()> {
                 
                     let bytes = append_vec.get_slice(vec_o, acc.stored_size);
                     let offset = {
-                        stream.borrow_mut().write(bytes.unwrap().0)?
+                        let mut stream = stream.borrow_mut();
+                        stream.write(bytes.unwrap().0)?
                     };
                     let program_id = pk2id32(&acc.account_meta.owner);
                     let acc_ref = WeakAccountRef { offset, program_id, slot };
