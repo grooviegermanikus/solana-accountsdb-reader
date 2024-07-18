18e7cd7ed83144dfa0294ec3c2510c5bf09156ac
GroovieGermanikus <groovie@mango.markets>
2024-07-18 10:24:52 +0200

    compile

--- src/bin/solana-accountsdb-reader/main.rs
+++ src/bin/solana-accountsdb-reader/main.rs
@@ -215,7 +215,7 @@ async fn main() -> anyhow::Result<()> {
     .custom_flags(libc::O_DIRECT)
     .open("bff")?;
 
-    let mut stream = AccountStreamFile {
+    let stream = AccountStreamFile {
         file,
         buffers: Box::new([AlignedBuffer([0u8; BUFFER_SIZE]); BUFFER_COUNT]),
         buffer_index: 0,
@@ -247,7 +247,8 @@ async fn main() -> anyhow::Result<()> {
                 
                     let bytes = append_vec.get_slice(vec_o, acc.stored_size);
                     let offset = {
-                        stream.write(bytes.unwrap().0)?
+                        let mut stream_mut = &stream;
+                        stream_mut.write(bytes.unwrap().0)?
                     };
                     let program_id = pk2id32(&acc.account_meta.owner);
                     let acc_ref = WeakAccountRef { offset, program_id, slot };
@@ -261,7 +262,10 @@ async fn main() -> anyhow::Result<()> {
         }
     }
 
-    stream.flush()?;
+    {
+        let mut stream_mut = &stream;
+        stream_mut.flush()?;
+    }
 
     info!(
         "... read {} append vecs in {}s items stored:{} bytes remaining:{}",
